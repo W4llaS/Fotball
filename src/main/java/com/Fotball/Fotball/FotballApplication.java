558edb213aec8fac6834858b739a1334617627e8
Walla <walla.valentim00@gmail.com>
2023-02-24 21:42:17 -0300

    Feat | Innital Commit

---
+++ src/main/java/com/Fotball/Fotball/FotballApplication.java
@@ -0,0 +1,12 @@
+package com.Fotball.Fotball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotballApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Fotball/Fotball/FotballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fotball.Fotball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
